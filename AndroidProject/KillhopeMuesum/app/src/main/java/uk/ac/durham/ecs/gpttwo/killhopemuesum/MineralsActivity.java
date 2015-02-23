8e2dc6f3d49dfc51192162ba7e4578367e784134
Alastair Breeze <me@alastairbreeze.com>
2015-02-23 17:22:41 +0000

    More Conflict Removals

--- AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuesum/MineralsActivity.java
+++ AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuesum/MineralsActivity.java
@@ -16,10 +16,7 @@ public class MineralsActivity extends ActionBarActivity {
 
     private GridView gridView;
     private MineralsAdapter adapter;
-<<<<<<< HEAD
-=======
     public static final long SPLASH_LENGTH = 2500l;
->>>>>>> master
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
