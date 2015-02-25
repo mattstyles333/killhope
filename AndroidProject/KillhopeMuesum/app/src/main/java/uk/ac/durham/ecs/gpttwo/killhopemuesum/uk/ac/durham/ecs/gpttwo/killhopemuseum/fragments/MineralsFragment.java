87a10263ea0c547f4c761cf03ad2a7ad4f9a07aa
Alastair Breeze <me@alastairbreeze.com>
2015-02-25 12:30:37 +0000

    Transitions To Mineral Activity
    Worked on the MineralsActivity adding transitions to each individual
    mineral.
    Started refining search for the MineralsActivity.
    Fixed some actionbar back bugs.
    Fixed incorrect string.

--- AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuesum/uk/ac/durham/ecs/gpttwo/killhopemuseum/fragments/MineralsFragment.java
+++ AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuesum/uk/ac/durham/ecs/gpttwo/killhopemuseum/fragments/MineralsFragment.java
@@ -1,24 +1,48 @@
 package uk.ac.durham.ecs.gpttwo.killhopemuesum.uk.ac.durham.ecs.gpttwo.killhopemuseum.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
+import uk.ac.durham.ecs.gpttwo.killhopemuesum.MineralActivity;
+import uk.ac.durham.ecs.gpttwo.killhopemuesum.MineralsAdapter;
 import uk.ac.durham.ecs.gpttwo.killhopemuesum.R;
 
 
-public class MineralsFragment extends Fragment {
+public class MineralsFragment extends Fragment implements AdapterView.OnItemClickListener {
+
+    private GridView gridView;
+    private MineralsAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_minerals, container, false);
+
+        gridView = (GridView) rootView.findViewById(R.id.minerals_gridview);
+
+        gridView.setOnItemClickListener(this);
+
+        adapter = new MineralsAdapter(getActivity());
+
+        gridView.setAdapter(adapter);
+
+
         return rootView;
     }
 
-    public static HistoryFragment newInstance(){
-        return new HistoryFragment();
+    public static MineralsFragment newInstance(){
+        return new MineralsFragment();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(getActivity(), MineralActivity.class);
+        startActivity(intent);
     }
 }
