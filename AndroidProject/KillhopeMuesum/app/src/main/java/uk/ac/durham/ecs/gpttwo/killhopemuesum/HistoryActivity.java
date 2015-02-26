7fa6bf30a86015201ba9a92c88950b28067eb6d9
mattstyles333 <mattstyles333@gmail.com>
2015-02-26 10:49:33 +0000

    Reworked history page to display unique layout files, added qr link from minerals page
    Note: help needs to be reworked similiarly

--- AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuesum/HistoryActivity.java
+++ AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuesum/HistoryActivity.java
@@ -10,26 +10,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import uk.ac.durham.ecs.gpttwo.killhopemuesum.fragments.HelpPageFragment;
-import uk.ac.durham.ecs.gpttwo.killhopemuesum.fragments.HistoryFragment;
 import uk.ac.durham.ecs.gpttwo.killhopemuesum.fragments.HistoryPageFragment;
 
 public class HistoryActivity extends ActionBarActivity {
 
-
-    public static final int NUM_PAGES = 5;
-
     private ViewPager mPager;
-
     private PagerAdapter mPageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_help_page);
+        setContentView(R.layout.activity_history_page);
 
-        mPager = (ViewPager)findViewById(R.id.help_pager);
+        mPager = (ViewPager)findViewById(R.id.history_pager);
         mPageAdapter = new HistoryPagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPageAdapter);
 
@@ -44,9 +38,6 @@ public class HistoryActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -61,7 +52,7 @@ public class HistoryActivity extends ActionBarActivity {
             }
             return true;
         }else if (id == R.id.action_nextpage) {
-            if(mPager.getCurrentItem() != NUM_PAGES - 1) {
+            if(mPager.getCurrentItem() != mPageAdapter.getCount() - 1) {
                 mPager.setCurrentItem(mPager.getCurrentItem() + 1);
             }
             return true;
@@ -77,19 +68,4 @@ public class HistoryActivity extends ActionBarActivity {
         super.onBackPressed();
     }
 
-    private class HistoryPagerAdapter extends FragmentStatePagerAdapter {
-        public HistoryPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return new HistoryPageFragment();
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
-    }
 }
