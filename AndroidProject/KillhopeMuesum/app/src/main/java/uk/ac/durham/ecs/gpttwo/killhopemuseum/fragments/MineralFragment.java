8110d6a705fee67f20cdfa53f06ee1a48ef0fe58
Alastair Breeze <me@alastairbreeze.com>
2015-03-26 01:00:16 +0000

    Added expandable
    Code needs cleaning up, depends on the final design. This is a temp
    demo.

--- AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuseum/fragments/MineralFragment.java
+++ AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuseum/fragments/MineralFragment.java
@@ -9,8 +9,10 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.abhi.gif.lib.AnimatedGifImageView;
@@ -79,58 +81,91 @@ public class MineralFragment extends Fragment {
         });
 
 
-        ListView listView = (ListView) rootView.findViewById(R.id.mineral_list);
-
-        MineralSectionAdapter adapter = new MineralSectionAdapter(getActivity(),mineral,true);
-
-        listView.setAdapter(adapter);
+//        ListView listView = (ListView) rootView.findViewById(R.id.mineral_list);
+//
+//        MineralSectionAdapter adapter = new MineralSectionAdapter(getActivity(),mineral,true);
+//
+//        listView.setAdapter(adapter);
+        LinearLayout list = (LinearLayout) rootView.findViewById(R.id.mineral_list);
+        list.addView(getNextView(0, null, null));
 
         return rootView;
     }
 
-//    public View getView(int position, ViewGroup parent) {
-//        LayoutInflater inflator = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//
-//        View view = inflator.inflate(R.layout.fragment_mineral_list_item,parent,false);
-//
-//        final int mineralID = getActivity().getIntent().getExtras().getInt("mineralID");
-//
-//        final Mineral mineral = ((KillhopeApplication)getActivity().getApplication()).mineralManager.getMineral(mineralID);
-//
-////        ListView listView = (ListView) view.findViewById(R.id.mineral_sublist);
-//
-////        MineralSubSectionAdapter adapter = new MineralSubSectionAdapter(mContext,mineral.getMineralSection(position));
-//
-////        listView.setAdapter(adapter);
-//
-//        TextView sectionName = (TextView)view.findViewById(R.id.section_name);
-//        sectionName.setText("Section " + (position + 1));
-//
-//        MineralSection ms = mineral.getMineralSection(position);
-//
-//        LinearLayout listView = (LinearLayout) view.findViewById(R.id.mineral_sublist);
-//
-////        ArrayList<String> glossaryTerms = mContext
-//
-//        for(int i=0;i<ms.getCount();i++) {
-//            View subview = inflator.inflate(R.layout.fragment_mineral_list_item_sub, parent, false);
-//
-//            TextView title = (TextView) subview.findViewById(R.id.mineral_sub_title);
-//            TextView desc = (TextView) subview.findViewById(R.id.mineral_sub_description);
-//
-//            String titleText = ms.getSub(i).getTitle();
-//            String descText = ms.getSub(i).getInfo();
-//
-//            descText = descText.replace("and", "<b>and</b>");
-//
-//            title.setText(Html.fromHtml(titleText));
-//            desc.setText(Html.fromHtml(descText));
-//
-//            listView.addView(subview);
-//        }
-//
-//        return view;
-//    }
+    public View getNextView(final int position, final ImageButton prevReduceButton, final ImageButton prevExpandButton) {
+        LayoutInflater inflator = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        final View view = inflator.inflate(R.layout.fragment_mineral_list_item,new LinearLayout(getActivity()),false);
+
+        final int mineralID = getActivity().getIntent().getExtras().getInt("mineralID");
+
+        final Mineral mineral = ((KillhopeApplication)getActivity().getApplication()).mineralManager.getMineral(mineralID);
+
+//        ListView listView = (ListView) view.findViewById(R.id.mineral_sublist);
+
+//        MineralSubSectionAdapter adapter = new MineralSubSectionAdapter(mContext,mineral.getMineralSection(position));
+
+//        listView.setAdapter(adapter);
+
+        TextView sectionName = (TextView)view.findViewById(R.id.section_name);
+        sectionName.setText("Section " + (position + 1));
+
+        MineralSection ms = mineral.getMineralSection(position);
+
+        LinearLayout listView = (LinearLayout) view.findViewById(R.id.mineral_sublist);
+
+//        ArrayList<String> glossaryTerms = mContext
+
+        for(int i=0;i<ms.getCount();i++) {
+            View subview = inflator.inflate(R.layout.fragment_mineral_list_item_sub, new RelativeLayout(getActivity()), false);
+
+            TextView title = (TextView) subview.findViewById(R.id.mineral_sub_title);
+            TextView desc = (TextView) subview.findViewById(R.id.mineral_sub_description);
+
+            String titleText = ms.getSub(i).getTitle();
+            String descText = ms.getSub(i).getInfo();
+
+            //placeholder, soon to be the whole glossary
+            descText = descText.replace("and", "<b>and</b>");
+
+            title.setText(Html.fromHtml(titleText));
+            desc.setText(Html.fromHtml(descText));
+
+            listView.addView(subview);
+        }
+
+        final ImageButton reduceButton = (ImageButton)view.findViewById(R.id.button_reduce);
+        if(position == 0){
+            reduceButton.setVisibility(View.GONE);
+        }
+        reduceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((LinearLayout)v.getParent().getParent()).removeView(view);
+                if(prevReduceButton!=null && position > 1){
+                    prevReduceButton.setVisibility(View.VISIBLE);
+                }
+                if(prevExpandButton!=null){
+                    prevExpandButton.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        final ImageButton expandButton = (ImageButton)view.findViewById(R.id.button_expand);
+        if(position > 2){
+            expandButton.setVisibility(View.GONE);
+        }
+        expandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout nextLevel = (LinearLayout)getView().findViewById(R.id.mineral_list);
+                nextLevel.addView(getNextView(position + 1,reduceButton,expandButton));
+                reduceButton.setVisibility(View.GONE);
+                expandButton.setVisibility(View.GONE);
+            }
+        });
+
+        return view;
+    }
 
     public static MineralFragment newInstance() {
         return new MineralFragment();
