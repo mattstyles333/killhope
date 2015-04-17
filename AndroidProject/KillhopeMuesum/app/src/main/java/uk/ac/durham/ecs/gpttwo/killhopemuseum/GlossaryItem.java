960b31cedaa66d4918e44ead7b94ab2593529095
Belob22 <rgrc22@gmail.com>
2015-04-17 20:49:44 +0100

    Revert "Revert "Potential changes to glossary search""
    This reverts commit 7f95229ea315a4261c1a089205ff11806911f539.

--- AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuseum/GlossaryItem.java
+++ AndroidProject/KillhopeMuesum/app/src/main/java/uk/ac/durham/ecs/gpttwo/killhopemuseum/GlossaryItem.java
@@ -53,7 +53,7 @@ public class GlossaryItem {
         return info;
     }
 
-    public void sortSubs()
+    public void sortSubsAlphabetically()
     {
         ArrayList<GlossaryItem> listToSort = (ArrayList<GlossaryItem>) subs.clone();
         Collections.sort(listToSort, new Comparator<GlossaryItem>() {
@@ -61,6 +61,19 @@ public class GlossaryItem {
                 final String glossary1 = o1.getName();
                 final String glossary2 = o2.getName();
                 return glossary1.compareTo(glossary2);
+            }
+        });
+    }
+
+    public void sortSubsByScore()
+    {
+        ArrayList<GlossaryItem> listToSort = (ArrayList<GlossaryItem>) subs.clone();
+        Collections.sort(listToSort, new Comparator<GlossaryItem>() {
+            public int compare(GlossaryItem o1, GlossaryItem o2) {
+                final int glossary1 = o1.getSearchScore();
+                final int glossary2 = o2.getSearchScore();
+                return glossary2 > glossary1 ? 1
+                        : glossary2 < glossary1 ? -1 : 0;
             }
         });
     }
